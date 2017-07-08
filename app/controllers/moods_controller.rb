2abfab1a91df6374aa59f9074bdf58e36d69654f
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-07-07 22:17:16 -0300

    Add create mood to team

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,16 +1,27 @@
 class MoodsController < ApplicationController
-  before_action :set_status, only: :new
+  before_action :set_status, only: [:new, :create]
 
   def new
     @mood = Mood.new
   end
 
   def create
+    @mood = current_user.team.moods.build(status: mood_params[:status])
+
+    if @mood.save
+      redirect_to root_path, status: :ok
+    else
+      render template: "shared/500.html", status: :internal_server_error
+    end
   end
 
   private
 
   def set_status
-    @status = Mood.statuses.to_a
+    @status = Mood.statuses.keys
+  end
+
+  def mood_params
+    params.require(:mood).permit(:status, :teams_id)
   end
 end
